bf05a152a0d784f6e839f0ac849c4ec604833877
David Stevens <dstevens@kth.se>
2020-04-30 20:57:35 +0200

    Add jokes

---
+++ docker-git/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "-----
+Some people, when confronted with a problem, think, 'I know, I'll use threads' - and then two they hav erpoblesm.
+-----
+'Knock knock.' 'Race condition.' 'Who's there?'
+-----
+I've got a really good UDP joke to tell you, but I don't know if you'll get it
+-----
+A manager, a mechanical engineer, and software analyst are driving back from convention through the mountains. Suddenly, as they crest a hill, the brakes on the car go out and they fly careening down the mountain. After scraping against numerous guardrails, they come to a stop in the ditch. Everyone gets out of the car to assess the damage.
+
+The manager says, "Let\''s form a group to collaborate ideas on how we can solve this issue."
+
+The mechanical engineer suggests, "We should disassemble the car and analyze each part for failure."
+
+The software analyst says, "Let'\''s push it back up the hill and see if it does it again."
+-----' > jokes.txt
